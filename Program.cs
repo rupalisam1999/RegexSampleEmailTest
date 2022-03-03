74c9c8fd18cb026fb087d9f22aa7d7c3cb520227
rupalisam1999 <rupalisangale1999@gmail.com>
2022-03-03 16:03:02 +0530

    ADDED: Sample eamil test for valid or invalid email

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace RegexforEmail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            UserRegistrastion userRegistrastion = new UserRegistrastion();
+            Console.WriteLine("Email" +userRegistrastion.ValidateEmail("abc@yahoo.com"));
+            Console.WriteLine("Email" + userRegistrastion.ValidateEmail("abcyahoo.com"));
+
+            Console.WriteLine("Email" + userRegistrastion.ValidateEmail("abc-100@yahoo.com"));
+            Console.WriteLine("Email" + userRegistrastion.ValidateEmail("abc-100yahoo.com"));
+
+            Console.WriteLine("Email" + userRegistrastion.ValidateEmail("abc.100@yahoo.com"));
+            Console.WriteLine("Email" + userRegistrastion.ValidateEmail("abc100yahoo.com"));
+
+            Console.WriteLine("Email" + userRegistrastion.ValidateEmail("abc111@abc.com"));
+            Console.WriteLine("Email" + userRegistrastion.ValidateEmail("abc111@abccom"));
+
+            Console.WriteLine("Email" + userRegistrastion.ValidateEmail("abc.100@abc.com.au"));
+            Console.WriteLine("Email" + userRegistrastion.ValidateEmail("abc100@abc.com.au"));
+
+            Console.WriteLine("Email" + userRegistrastion.ValidateEmail("abc@1.com"));
+            Console.WriteLine("Email" + userRegistrastion.ValidateEmail("abc.100@abc.com.au"));
+
+            Console.WriteLine("Email" + userRegistrastion.ValidateEmail("abc+100@gmail.com"));
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ UserRegistrastion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexforEmail
+{
+    class UserRegistrastion
+
+    {
+        const string EMAIL_PATTERN = "^[a-z]{2,}[+][0-9]{1,3}[@][a-z]{3,}[.][a-z]{3,}$";
+       
+
+        public bool ValidateEmail(string email)
+        {
+            var result = Regex.Match(email, EMAIL_PATTERN);
+            if (result.Success)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+       
+    }
+}
